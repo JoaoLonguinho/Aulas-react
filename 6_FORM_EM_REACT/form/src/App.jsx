b83575b7da702357d231b9db3192207fbc417c97
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2024-09-09 23:40:41 -0300

    Continuação formulários

--- 6_FORM_EM_REACT/form/src/App.jsx
+++ 6_FORM_EM_REACT/form/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css';
-import MyForm from './components/MyForm';
+import "./App.css";
+import MyForm from "./components/MyForm";
 
 function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm />
+      <MyForm
+        user={{
+          name: "João",
+          email: "joao.pedro7111@hotmail.com",
+          bio: "Sou um dev",
+          role: "Estudante",
+        }}
+      />
     </div>
   );
 }
--- 6_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 6_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -1,41 +1,80 @@
-import { useState } from 'react'
-import './MyForm.css'
-function MyForm() {
+import { useState } from "react";
+import "./MyForm.css";
+function MyForm({ user }) {
+  // Controlled inputs
   // 3 - Gerenciamento de dados
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
+
+  const [bio, setBio] = useState(user ? user.bio : "");
+  const [role, setRole] = useState(user ? user.role : "");
 
   const handleName = (e) => {
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('Enviando o formulário')
-    console.log(name, email)
-  }
+    e.preventDefault();
+    console.log("Enviando o formulário");
+    console.log(name, email, bio, role);
+    // Ficaria aqui abaixo:
+    // Validação
+    // 7 Limpando inputs
+    // Envio
+    setName("");
+    setEmail("");
+  };
 
   return (
-
-
     <div>
       {/* 5 - envio de form */}
-        {/* 1. Criação de form */}
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" id="" placeholder='Digite o seu nome'onChange={handleName}/>
-            </div>
-            {/* 2. Label envolvendo input */}
-            <label>
-                <span>E-mail:</span>
-                {/* Simplificação de manipulação de e-mail usando State */}
-                <input type="email" name="email" placeholder='Digite seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
+      {/* 1. Criação de form */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            id=""
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+            value={name}
+          />
+        </div>
+        {/* 2. Label envolvendo input */}
+        <label>
+          <span>E-mail:</span>
+          {/* Simplificação de manipulação de e-mail usando State */}
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        {/* 8 - textarea */}
+        <label htmlFor="">
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        {/* 9 - Select */}
+        <label htmlFor="">
+          <span>Função no sistema</span>
+          <select name="role" onChange={(e) => setRole(e.target.value)}>
+            <option value="estudante">Estudante</option>
+            <option value="trabalhador">Trabalhador</option>
+          </select>
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default MyForm
+export default MyForm;
