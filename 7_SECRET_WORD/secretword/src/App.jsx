e0c14e185c70ee3f5ab420ea91b2265511d9315e
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2024-10-03 21:02:12 -0300

    Criação de funcionamento e estilização de tela do jogo

--- 7_SECRET_WORD/secretword/src/App.jsx
+++ 7_SECRET_WORD/secretword/src/App.jsx
@@ -22,8 +22,40 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordList)
 
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
+
+const pickWordAndCategory = () => {
+
+  //pegar a categoria
+  const categories = Object.keys(words) // chaves das categorias do words.js
+  const category = categories[Math.floor(Math.random() * Object.keys(categories).length )]
+
+  console.log(category)
+
+  //pegar a palavra
+  const word = words[category][Math.floor(Math.random() * words[category].length )]
+
+  console.log(word)
+
+  return {word, category}
+}
   //Inicia o game
   const startGame = () =>{
+
+
+    const {word, category} = pickWordAndCategory()
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    console.log(word, category, wordLetters)
+
+
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+
     setGameStage(stages[1].name)
   }
 
--- 7_SECRET_WORD/secretword/src/components/Game.jsx
+++ 7_SECRET_WORD/secretword/src/components/Game.jsx
@@ -1,8 +1,30 @@
 import './Game.css'
 function Game({verifyLetter}) {
   return (
-    <div>
-      <button onClick={verifyLetter}>Continuar o jogo</button>
+    <div className="game">
+      <div className="points">
+        <span>Pontuação: ---</span>
+      </div>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Tentar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Tentativas passadas: </p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
