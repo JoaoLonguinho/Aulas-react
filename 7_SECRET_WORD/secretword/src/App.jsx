f57c0031b86387682299de771a645bd747602ed2
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2024-12-10 21:39:48 -0300

    Adicionando validação de letras e atualizando elas no jogo

--- 7_SECRET_WORD/secretword/src/App.jsx
+++ 7_SECRET_WORD/secretword/src/App.jsx
@@ -68,7 +68,22 @@ const pickWordAndCategory = () => {
   // rocess the letter input
   const verifyLetter = (letter) =>{
     console.log(letter)
+    const normalizedLetter = letter.toLowerCase();
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+      return;
+    }
 
+    if(letters.includes(normalizedLetter)){
+        setGessedLetters((actualGuessedLetters ) => [
+          ...actualGuessedLetters, normalizedLetter
+        ])
+    } else {
+      setWrongLetters((actualWrongLetters ) => [
+        ...actualWrongLetters, normalizedLetter
+      ])
+    }
+    console.log(guessedLetters)
+    console.log(wrongLetters)
   }
   const restartGame = () =>{
     setGameStage(stages[0].name)
