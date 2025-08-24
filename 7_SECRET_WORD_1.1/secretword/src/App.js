b9c3d24d0ddcbb39e70f95c5632b1e2646034379
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-08-24 11:42:55 -0300

    Function to change screens

--- 7_SECRET_WORD_1.1/secretword/src/App.js
+++ 7_SECRET_WORD_1.1/secretword/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import StartScreen from './components/StartScreen';
+import MidScreen from './components/MidScreen';
+import EndScreen from './components/EndScreen';
 import { useCallback, useEffect, useState } from 'react';
 import { wordsList } from './data/words';
 
@@ -18,9 +20,15 @@ const stages = [
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);  // hook puxando o primeiro estágio do jogo
+  const [words] = useState(wordsList); // Busca as palavras no arquivo words
+  const startGame = () => {
+    setGameStage(stages[1].name); // stages -> o array de objetos, 1 -> posição, name -> o dado dentro do objeto do array que está sendo alterado
+  }
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'mid' && <MidScreen/>}
+      {gameStage === 'end' && <EndScreen/>}
     </div>
   );
 }
---
+++ 7_SECRET_WORD_1.1/secretword/src/components/MidScreen/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MidScreen = () => {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default MidScreen
