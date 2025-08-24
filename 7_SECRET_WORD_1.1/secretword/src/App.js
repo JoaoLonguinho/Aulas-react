fa84658500a95191d5ea7c0ba73679189b7e1c06
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-08-24 11:30:56 -0300

    Adding code to change stages

--- 7_SECRET_WORD_1.1/secretword/src/App.js
+++ 7_SECRET_WORD_1.1/secretword/src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 import StartScreen from './components/StartScreen';
+import { useCallback, useEffect, useState } from 'react';
+import { wordsList } from './data/words';
+
+const stages = [
+  {
+    id: 1, name: "start"
+  },
+  {
+    id: 2, name: "mid"
+  },
+  {
+    id: 3, name: "end"
+  }
+]
+
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);  // hook puxando o primeiro estágio do jogo
   return (
     <div className="App">
       <StartScreen />
