ed9d2505d27d1f62ad134d1a227d96323c08d76a
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-08 21:15:09 -0300

    react router

---
+++ 9_REACT_ROUTER/react-router/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+// 1- Configuração de routes 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// 2- importanto páginas
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
