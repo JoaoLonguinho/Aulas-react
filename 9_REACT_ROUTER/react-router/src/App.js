a5dfd5291681140f96b8054f9e2ad238a2c280bd
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-08 21:26:44 -0300

    adding navbar

--- 9_REACT_ROUTER/react-router/src/App.js
+++ 9_REACT_ROUTER/react-router/src/App.js
@@ -7,11 +7,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+// 3-Componentes
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar /> {/* É obrigatório colocar dentro do BrowserRouter */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/about' element={<About />} />
--- 9_REACT_ROUTER/react-router/src/pages/Home.jsx
+++ 9_REACT_ROUTER/react-router/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      
+      <h2>
+          Home
+      </h2>
     </div>
   )
 }
