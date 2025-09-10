c7251d66bb27a74df4b1efaf69f8494b6054f443
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-09 21:40:06 -0300

    charging dynamic data using custom hook

--- 9_REACT_ROUTER/react-router/src/pages/Products.js
+++ 9_REACT_ROUTER/react-router/src/pages/Products.js
@@ -1,13 +1,30 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch";
+
 
 function Products() {
   // 4- rota dinâmica
 
   const { id } = useParams(); //Desestruturação de objeto
 
+  // 5- carregamento dado individual 
+
+  const url = "http://localhost:3000/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
+
+  console.log(product);
+
   return (
     <>
       <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h2>{product.name}</h2>
+          <h3>R$ {product.price}</h3>
+        </div>
+      )}
     </>
   )
 }
