81a66e89b71bddaec9ff048760623287dd8e0b1f
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-16 21:20:57 -0300

    context and provider

--- 11_CONTEXT_API/context-api/src/context/CounterContext.jsx
+++ 11_CONTEXT_API/context-api/src/context/CounterContext.jsx
@@ -1,4 +1,15 @@
 // 1- criar contexto 
 import { createContext, useState } from "react";
 
-export default CounterContext = createContext();
+const CounterContext = createContext(); // Necessário criar e atribuir antes de exportar.
+export default CounterContext;
+
+// 2- provider 
+export const CounterContextProvider = ({children}) => {
+    const [counter, setCounter] = useState(5);
+    return (
+        <CounterContext.Provider value={{counter, setCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
