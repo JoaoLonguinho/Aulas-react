539dbd777a4441628fab9e8af204a4d5d07b5cb2
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-23 20:47:19 -0300

    More complex context

--- 11_CONTEXT_API/context-api/src/pages/Home.jsx
+++ 11_CONTEXT_API/context-api/src/pages/Home.jsx
@@ -2,13 +2,16 @@
 import { useCounterContext } from "../hooks/useCounterContext";
 // import { CounterContext } from "../context/CounterContext";
 import ChangeCounter from "../components/ChangeCounter";
-
+// 5- context complexo
+import { useTitleColorContext } from "../hooks/UseTitleColorContextProvider";
 const Home = () => {
   const {counter} = useCounterContext();
+
+  const {color} = useTitleColorContext();
   // const { counter } = useContext(CounterContext);
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{color:color}}>Home</h1>
       <p>Valor do contador: {counter}</p>
       <ChangeCounter />
     </div>
