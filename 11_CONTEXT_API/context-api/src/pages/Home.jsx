c8bb7975a4577c2f476d7b69cc5c602ddfb70ce5
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-09-23 21:10:59 -0300

    using the dispatch

--- 11_CONTEXT_API/context-api/src/pages/Home.jsx
+++ 11_CONTEXT_API/context-api/src/pages/Home.jsx
@@ -5,15 +5,25 @@ import ChangeCounter from "../components/ChangeCounter";
 // 5- context complexo
 import { useTitleColorContext } from "../hooks/UseTitleColorContextProvider";
 const Home = () => {
-  const {counter} = useCounterContext();
+  const { counter } = useCounterContext();
 
-  const {color} = useTitleColorContext();
+  const { color, dispatch } = useTitleColorContext();
   // const { counter } = useContext(CounterContext);
+
+  // 6- alterando state complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color })
+  }
   return (
     <div>
-      <h1 style={{color:color}}>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Valor do contador: {counter}</p>
       <ChangeCounter />
+      {/* 6- alterando state complexo */}
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
     </div>
   )
 }
